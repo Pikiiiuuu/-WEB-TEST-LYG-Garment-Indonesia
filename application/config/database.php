cd31ed677bb249200b4f1c5e5f476dbb4ff2f5a4
Pikiiiuuu <fiqiandrireviansyah@gmail.com>
2024-01-04 21:56:09 +0700

    Configuring database with local SQL Server

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+//configuring database with Local SQL Server
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'LAPTOP-KN7ILVIJ\SQLEXPRESS',
