ca676f2d924daa7b7a2e5127b47f26e6b49e0c0a
Pikiiiuuu <fiqiandrireviansyah@gmail.com>
2024-01-04 22:33:53 +0700

    Create model and get sewing data from database

--- application/models/M_Sewing.php
+++ application/models/M_Sewing.php
@@ -7,4 +7,16 @@ class M_Sewing extends CI_Model {
 		parent::__construct();
 		$this->load->database();
 	}
+	//get sewing data from database
+	public function listing()
+	{
+		$this->db->select('a.trnDate, a.styleCode, COUNT(a.SizeName) output, COUNT(b.SizeSort) as size');
+		$this->db->from('dbo.lygSewingOutput a');
+		// JOIN
+		$this->db->join('dbo.lygStyleSize b', 'b.StyleCode = a.StyleCode', 'left');
+		// END JOIN
+		$this->db->group_by('a.TrnDate, a.StyleCode');
+		$query	= $this->db->get();
+		return $query->result();
+	}
 }
